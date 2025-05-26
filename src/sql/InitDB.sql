c4a57304147b2dd403fbc799aab27926777c14f4
Lukas Müller <lukastm@mail.uni-paderborn.de>
2025-05-26 15:40:42 +0200

    Changes in db schema and some init data

--- src/sql/InitDB.sql
+++ src/sql/InitDB.sql
@@ -1,8 +1,3 @@
-CREATE TABLE language (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL UNIQUE
-);
-
 CREATE TABLE region (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL UNIQUE
@@ -17,9 +12,7 @@ CREATE TABLE game (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     console_id INTEGER,
-    region_id INTEGER,
-    FOREIGN KEY (console_id) REFERENCES console(id),
-    FOREIGN KEY (region_id) REFERENCES region(id)
+    FOREIGN KEY (console_id) REFERENCES console(id)
 );
 
 CREATE TABLE genre (
@@ -32,18 +25,18 @@ CREATE TABLE track (
     title TEXT NOT NULL,
     interpret TEXT NOT NULL,
     year INTEGER,
-    length INTEGER,  -- Length in seconds
-    url TEXT,
-    language_id INTEGER,
-    FOREIGN KEY (language_id) REFERENCES language(id)
+    length TEXT,
+    url TEXT
 );
 
 CREATE TABLE track_game (
     track_id INTEGER,
     game_id INTEGER,
-    PRIMARY KEY (track_id, game_id),
+    region_id INTEGER,
+    PRIMARY KEY (track_id, game_id, region_id),
     FOREIGN KEY (track_id) REFERENCES track(id),
-    FOREIGN KEY (game_id) REFERENCES game(id)
+    FOREIGN KEY (game_id) REFERENCES game(id),
+    FOREIGN KEY (region_id) REFERENCES region(id)
 );
 
 CREATE TABLE track_genre (
@@ -53,3 +46,58 @@ CREATE TABLE track_genre (
     FOREIGN KEY (track_id) REFERENCES track(id),
     FOREIGN KEY (genre_id) REFERENCES genre(id)
 );
+
+-- Insert regions
+INSERT INTO region (name) VALUES ('UK');
+INSERT INTO region (name) VALUES ('NA');
+INSERT INTO region (name) VALUES ('DE');
+INSERT INTO region (name) VALUES ('ES');
+INSERT INTO region (name) VALUES ('FR');
+INSERT INTO region (name) VALUES ('IT');
+INSERT INTO region (name) VALUES ('PL');
+INSERT INTO region (name) VALUES ('SE');
+INSERT INTO region (name) VALUES ('NL');
+INSERT INTO region (name) VALUES ('AU');
+INSERT INTO region (name) VALUES ('PT');
+INSERT INTO region (name) VALUES ('DK');
+INSERT INTO region (name) VALUES ('FI');
+INSERT INTO region (name) VALUES ('NO');
+INSERT INTO region (name) VALUES ('HR');
+INSERT INTO region (name) VALUES ('BE');
+
+-- Insert consoles
+INSERT INTO console (name) VALUES ('PS2');
+INSERT INTO console (name) VALUES ('PS3');
+INSERT INTO console (name) VALUES ('PS4');
+INSERT INTO console (name) VALUES ('PS5');
+
+-- Insert PS2 games
+INSERT INTO game (name, console_id) VALUES ('SingStar', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar 80s', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar 90s', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar ABBA', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Amped', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Anthems', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Boy Bands vs Girl Bands', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Chart Hits', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Country', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Hottest Hits', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Legends', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Motown', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Party', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Pop', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Pop Hits', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Pop Vol. 2', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Queen', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar R&B', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Rock Ballads', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Rocks!', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Singalong with Disney', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Studio 100', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Summer Party', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Take That', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar The Wiggles', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar The Dome', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Schlager', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Deutsch Rock-Pop', 1);
+INSERT INTO game (name, console_id) VALUES ('SingStar Deutsch Rock-Pop Vol. 2', 1);
